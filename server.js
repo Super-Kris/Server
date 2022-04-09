c3b361621a4c57c7026c7c21716a6aee64a82608
Super-Kris <gimlisonofgloin@tlen.pl>
2022-04-09 14:35:35 +0200

    nowy package.json

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 8080;
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Krzysztof jest super SUPER!');
+  res.end('Krzysztof jest SUPER!');
 }
 const server = http.createServer(requestListener);
 server.listen(port);
